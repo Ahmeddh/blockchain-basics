61e2b14eafbf4faea53a932f860afd8d73eeefa0
ahmeddh <mr.ahmeddh@hotmail.com>
2022-10-06 22:32:51 +0200

    cleanup calc js and update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,14 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-    const HelloWorld= await ethers.getContractFactory("HelloWorld");
-    const hello_world= await HelloWorld.deploy("Hello Blockchain");
+    // const HelloWorld= await ethers.getContractFactory("HelloWorld");
+    // const hello_world= await HelloWorld.deploy("Hello Blockchain");
 
-    console.log("Contract has been deployed to address: ",hello_world.address);
+    // console.log("Contract has been deployed to address: ",hello_world.address);
+    const Math= await ethers.getContractFactory("Math");
+    const math= await Math.deploy();
 
+    console.log("Contract has been created at: "+math.address);
 }
 
 main()
