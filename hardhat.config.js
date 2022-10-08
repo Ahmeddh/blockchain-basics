b1e19fc7370e0fcf6043e2fbae95b1d495c7bf7c
ahmeddh <mr.ahmeddh@hotmail.com>
2022-10-09 00:03:56 +0200

    call contract from another contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ require('@nomiclabs/hardhat-ethers');
 const { API_URL ,PRIVATE_KEY } = process.env; 
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.8",
   defaultNetwork: "maticmum",
   networks:{
     hardhat:{},
