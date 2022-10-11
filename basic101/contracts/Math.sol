b85a66c85d8f2625835b7f9a4f565bd2c8698d5b
ahmeddh <mr.ahmeddh@hotmail.com>
2022-10-11 05:17:34 +0200

    wrap project into a folder to create more project in the same repo

---
+++ basic101/contracts/Math.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.0;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract Math {
+    uint256 results;
+    address public owner;
+    event addNumbers(uint256 firstNo, uint256 secondNo, uint256 results);
+    
+    constructor (){
+        owner=msg.sender;
+    }
+    
+    function add(uint256 a,uint256 b)public returns (uint256){
+        results=a+b;
+        emit addNumbers(a, b, results);
+
+        return results;
+    }
+}
