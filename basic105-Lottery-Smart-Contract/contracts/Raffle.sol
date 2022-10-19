e5f3f85438b83356d0cb4cd29599bb3a8bb7306e
ahmeddh <mr.ahmeddh@hotmail.com>
2022-10-19 19:59:17 +0200

    Initial commit

---
+++ basic105-Lottery-Smart-Contract/contracts/Raffle.sol
@@ -0,0 +1,13 @@
+// Raffle
+
+// Enter the lottery (Pay ticket fees)
+// Pick a random winner (verifiablly random)
+// Winner to be selected every x minutes automatically
+// Chainlink Oracle -> Randomness, Automatic execution (Chainlink Keeper)
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract Raffle {
+  constructor() {}
+}
