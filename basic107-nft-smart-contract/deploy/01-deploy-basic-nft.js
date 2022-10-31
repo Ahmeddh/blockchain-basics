9dc0673835cc07415978f772ea72d1ace9d33d83
ahmeddh <mr.ahmeddh@hotmail.com>
2022-10-31 09:57:26 +0200

    basic NFT contract

---
+++ basic107-nft-smart-contract/deploy/01-deploy-basic-nft.js
@@ -0,0 +1,22 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ deployments, getNamedAccounts }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("---------------------------------------------------")
+    const arguments = []
+    const basicNFT = await deploy("BasicNFT", {
+        from: deployer,
+        args: arguments,
+        log: true,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying.....")
+        await verify(basicNFT.address, arguments)
+        log("---------------------------------------------------")
+    }
+}
