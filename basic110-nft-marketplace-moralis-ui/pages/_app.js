f4e86c3d4f170f899b4909e93706fc3d3de9ed53
ahmeddh <mr.ahmeddh@hotmail.com>
2022-11-13 11:38:15 +0200

    add styles

--- basic110-nft-marketplace-moralis-ui/pages/_app.js
+++ basic110-nft-marketplace-moralis-ui/pages/_app.js
@@ -1,7 +1,25 @@
 import "../styles/globals.css"
+import Head from "next/head"
+import Header from "../components/Header"
+import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "web3uikit"
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="Minimalistic NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <NotificationProvider>
+                    <Header />
+                    <Component {...pageProps} />
+                </NotificationProvider>
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
